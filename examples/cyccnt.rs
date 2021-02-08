5a0c72e5667b05e06bcd49f5ed9c0f39acae7e0b
Mark Hakansson <mark.hakansson@live.se>
2021-02-08 08:06:50 +0100

    Test project for rauk

---
+++ examples/cyccnt.rs
@@ -0,0 +1,28 @@
+// cyccnt.rs
+
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+
+#[cfg(not(feature = "klee-analysis"))]
+use panic_halt as _;
+#[cfg(feature = "klee-analysis")]
+use panic_klee as _;
+
+#[no_mangle]
+fn main() {
+    let mut core = cortex_m::Peripherals::take().unwrap();
+    // core.DCB.enable_trace();
+    // core.DWT.enable_cycle_counter();
+
+    let start: u32 = core.DWT.cyccnt.read();
+
+    // some code to measure
+    // ...
+
+    let end = core.DWT.cyccnt.read();
+
+    let _time = end.wrapping_sub(start);
+    //let _time = end - start;
+}
