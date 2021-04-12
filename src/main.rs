32fc023ecfd26faec2acbdce13da396ce2768595
Mark Hakansson <mark.hakansson@live.se>
2021-04-12 15:30:51 +0200

    Update test harness

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![no_main]
 #![no_std]
-//#![feature(asm)]
 
 #[cfg(not(feature = "klee-analysis"))]
 use panic_halt as _;
@@ -9,7 +8,7 @@ use panic_klee as _;
 
 #[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true, dispatchers = [USART1])]
 mod app {
-    use cortex_m::asm;
+    use stm32f4xx_hal::{gpio::*, prelude::*};
 
     #[resources]
     struct Resources {
@@ -17,18 +16,35 @@ mod app {
         #[init(1337)]
         b: u16,
         c: u16,
+        #[init([0; 1])]
+        buffer: [u32; 1],
+        dwt: stm32f4xx_hal::stm32::DWT,
     }
 
     #[init]
-    fn init(_cx: init::Context) -> (init::LateResources, init::Monotonics) {
+    fn init(mut cx: init::Context) -> (init::LateResources, init::Monotonics) {
         let value = 30;
-        (init::LateResources { a: value, c: 50 }, init::Monotonics {})
+        (
+            init::LateResources {
+                a: value,
+                c: 50,
+                dwt: cx.core.DWT,
+            },
+            init::Monotonics {},
+        )
     }
 
-    #[task(binds = EXTI0, resources = [a])]
+    #[no_mangle]
+    #[task(binds = EXTI0, resources = [a, c])]
     fn increment(mut cx: increment::Context) {
-        cx.resources.a.lock(|a| {
-            *a + 1;
+        let mut a = cx.resources.a;
+        let mut c = cx.resources.c;
+
+        a.lock(|a| {
+            c.lock(|c| {
+                *a + 1;
+                *c + 100;
+            });
         });
     }
 
@@ -38,11 +54,13 @@ mod app {
         cx.resources.b.lock(|b| *b - 10);
     }
 
-    // Software task not yet supported
-    // #[task(resources = [c])]
-    // fn software_task(mut cx: software_task::Context) {
-    //     cx.resources.c.lock(|c| *c + 50);
-    // }
+    #[task(binds = EXTI2, resources = [dwt, buffer])]
+    fn reading(mut cx: reading::Context) {
+        let start: u32 = cx.resources.dwt.lock(|dwt| dwt.cyccnt.read());
+        let end: u32 = cx.resources.dwt.lock(|dwt| dwt.cyccnt.read());
+        let time = end - start;
+        cx.resources.buffer.lock(|buffer| buffer[0] = time);
+    }
 
     #[idle]
     fn idle(_cx: idle::Context) -> ! {
